d5ea95accdcc5523e488307398de9eb3c878d3f6
Igor Prysyazhnyuk <igor@admins-imac-2.local>
2016-08-31 12:14:29 +0300

    Initial Commit

---
+++ ImageColorPicker/ImageColorPicker.h
@@ -0,0 +1,19 @@
+//
+//  ImageColorPicker.h
+//  ImageColorPicker
+//
+//  Created by Igor Prysyazhnyuk on 8/31/16.
+//  Copyright © 2016 Steelkiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageColorPicker.
+FOUNDATION_EXPORT double ImageColorPickerVersionNumber;
+
+//! Project version string for ImageColorPicker.
+FOUNDATION_EXPORT const unsigned char ImageColorPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageColorPicker/PublicHeader.h>
+
+
